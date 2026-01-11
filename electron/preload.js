27d159185b9b638543bd70e4ffcc5206149447d7
andrewzapps <your-email@example.com>
2026-01-10 23:50:55 -0500

    load

---
+++ electron/preload.js
@@ -0,0 +1,14 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI',
+{
+  startMacro: (data) => ipcRenderer.send('start-macro', data),
+  stopMacro: () => ipcRenderer.send('stop-macro'),
+  pauseMacro: () => ipcRenderer.send('pause-macro'),
+  
+  onMacroStarted: (callback) => ipcRenderer.on('macro-started', (event, data) => callback(data)),
+  onMacroStopped: (callback) => ipcRenderer.on('macro-stopped', (event, data) => callback(data)),
+  onMacroPaused: (callback) => ipcRenderer.on('macro-paused', (event, data) => callback(data)),
+  onMacroStatus: (callback) => ipcRenderer.on('macro-status', (event, data) => callback(data)),
+  onMacroError: (callback) => ipcRenderer.on('macro-error', (event, data) => callback(data))
+});
